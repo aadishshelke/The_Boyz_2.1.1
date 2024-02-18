3825e2873eb15d683c2395f9d287afcbce153dba
Aadish Shelke <shelkeaadish@gmail.com>
2024-02-18 12:17:37 +0530

    Second Commit

---
+++ social-media/src/components/Login.js
@@ -0,0 +1,209 @@
+import styled from "styled-components";
+
+const Login = (props) => {
+  return (
+    <Container>
+      <Nav>
+        <a href="/">
+          <img src="/images/logo-white2.png"  alt="" />
+        </a>
+        <div>
+          <Join>Join now</Join>
+          <SignIn>Sign in</SignIn>
+        </div>
+      </Nav>
+      <Section>
+        <Hero>
+          <h1>Welcome to your professional community</h1>
+          <img src="/images/login-hero_.svg" alt="" />
+        </Hero>
+        <Form>
+          <Started>
+            <img src="/images/BG_black.png" alt="" />
+            Get Started
+          </Started>
+          <Account>
+            Already have an account?<span><a href = "/">Log In</a></span>         
+          </Account>
+        <Text>
+            <p>Lorem idnw dwido wdiso wdii di wsidws wihdsidiws di wid weiie ie iwf eo w fje Lorem ipsum dolor ipmetr.</p>
+        </Text>
+        </Form>
+      </Section>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  padding: 0px;
+`;
+
+const Nav = styled.nav`
+  max-width: 100%;
+  margin-left : 5vh;
+  padding: 12px 0 16px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+
+  & > a {
+    height: 5vh;
+    @media (max-width: 768px) {
+      padding: 0 5px;
+    }
+    
+  }
+  @media (max-width: 768px) {
+    flex-wrap: wrap;
+    & > div{
+      margin: 3.5vh;
+    }
+  }
+  & > a > img {
+    height : 6vh;
+  }
+  
+`;
+
+const Join = styled.a`
+  font-size: 2.5vh;
+  padding: 10px 12px;
+  text-decoration: none;
+  border-radius: 4px;
+  color: rgba(0, 0, 0, 0.6);
+  margin-right: 12px;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+    color: rgba(0, 0, 0, 0.9);
+    text-decoration: none;
+  }
+`;
+
+const SignIn = styled.a`
+  box-shadow: inset 0 0 0 1px #2A2C35;
+  color: #2A2C35;
+  border-radius: 20px;
+  transition-duration: 167ms;
+  font-size: 2.5vh;
+  font-weight: 600;
+  line-height: 40px;
+  padding: 10px 24px;
+  text-align: center;
+  background-color: rgba(0, 0, 0, 0);
+  &:hover {
+    background-color: #ffcccc;
+    color: #000000  ;
+    text-decoration: none;
+  }
+`;
+
+const Section = styled.section`
+  display: flex;
+  align-content: start;
+  min-height: 400px;
+  padding-bottom: 138px;
+  padding-top: 40px;
+  padding: 60px 0;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1128px;
+  align-items: center;
+  margin: 5vh;
+  margin-bottom: 0vh;
+
+  @media (max-width: 768px) {
+    margin: auto;
+    min-height: 0px;
+  }
+`;
+
+const Hero = styled.div`
+  width: 100%;
+  h1 {
+    padding-bottom: 0;
+    width: 55%;
+    text-align : left;
+    font-size: 56px;
+    color: #2A2C35;
+    font-weight: 200;
+    line-height: 70px;
+    @media (max-width: 768px) {
+      text-align: center;
+      font-size: 20px;
+      width: 100%;
+      line-height: 2;
+    }
+  }
+
+  img {
+    width: 120vh;
+    height: 95vh;
+    position: absolute;
+    top: 0vh;
+    left: 94vh;
+    @media (max-width: 768px) {
+      top: 230px;
+      width: initial;
+      position: initial;
+      height: initial;
+    }
+  }
+`;
+
+const Form = styled.div`
+  margin-top: 100px;
+  width: 408px;
+  @media (max-width: 768px) {
+    margin-top: 20px;
+  }
+`;
+
+const Started = styled.button`
+  display: flex;
+  justify-content: center;
+  background-color: #ffd7d7;
+  align-items: center;
+  height: 56px;
+  width: 100%;
+  border-radius: 26px;
+  box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%),
+    inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 / 0);
+
+  vertical-align: middle;
+  z-index: 0;
+  transition-duration: 167ms;
+  font-size: 20px;
+  color: rgba(0, 0, 0, 0.6);
+  &:hover {
+    background-color: #ffacac;
+    color: rgba(0, 0, 0, 0.75);
+  }
+  & > img {
+    padding : 1vh;
+  } 
+ 
+`;
+
+const Text = styled.div`
+  padding-top: 100px;
+  text-align : left;
+  width: 100vh;
+  font-size : 3.5vh ;
+`;
+const Account = styled.div`
+  padding-top: 2vh;
+  padding-left: 10vh;
+  text-align : left;
+  width: 100vh;
+  font-size : 2.3vh ;
+  & > span > a {
+    text-decoration : none;
+    font-weight : 600;
+  }
+`;
+
+
+export default Login;
